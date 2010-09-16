624be0b82251e6352501fbaf3ba2eb4af23d3d8d
Daniel Karlsson <daniel@fraction4.com>
2010-09-17 05:48:08 +0800

    Trashed even more old code.

--- template.php
+++ template.php
@@ -1,13 +1,4 @@
 <?php
-/*
-funciton switchback_breadcrum($breadcrum){
-	return 'test';
-}
-*/
-
-
-/** * Add frontpage 'active' state handling to menu links * and section 'active' state handling when child is active * and menu-[title] as css id * * modified from code originally posted at * rogerlopez.net/handbook/primary_menu_tweaks * */ 
-
 
 function dws_menu_item_link($link) {
   if (empty($link['localized_options'])) {
@@ -27,6 +18,7 @@ function dws_preprocess_page(&$vars) {
 	}
 	$vars['primary_links'] = $links;
 }
+
 
 function phptemplate_breadcrumb($breadcrumb) {
     $title = drupal_get_title();
@@ -79,63 +71,5 @@ function phptemplate_breadcrumb($breadcrumb) {
 	}
   	return $crumbs;
 }
-
-/* ---- old 
-
-
-function phptemplate_breadcrumb($breadcrumb) {
-    $title = drupal_get_title();
-    
-	if (!empty($breadcrumb)) {
-		$lastitem = sizeof($breadcrumb);
-		$crumbs = '<ul id="breadcrum">';
-		$a=1;
-        
-	    // unset($breadcrumb[0]); // Remove the Drupal "home" item
-		// $crumbs .= '<li><a href="/"><span>Home</span></a></li>'; // Add RSR home item
-		//$lastitem--;
-	
-		foreach($breadcrumb as $value) {
-			if ($a!=$lastitem) {
-				$bits = explode('>', $value);
-				$out1 = '';
-			
-				$a=1;
-		    	foreach($bits as $part) {
-					if ($a==1) {
-						$out1 .= $part . '><span>';
-					}
-					if ($a==2) {
-						$slices = explode('<', $part);
-
-						$out2 = '';
-						$b = 1;
-						foreach($slices as $fraction) {
-							if($b==1) {
-								$out2 .= $fraction . '</span></a>';  
-							}
-							$b++;
-						}
-						$out1 .= $out2;
-					}
-					$a++;
-				}
-				$crumbs .= '<li>' . $out1 . '</li>';
-         		$a++;
-			}
-			else {
-				$crumbs .= '<li class="breadcrumb-last">'.$value.'</li>';
-			}
-		}
-		$crumbs .= '<li id="last_breadcrum_item">'.$title.'</li>';
-		$crumbs .= '</ul>';
-	}
-	else {
-		$crumbs = '<ul id="breadcrum"><li><a href="/"><span>Home</span></a></li><li id="last_breadcrum_item">' .$title. '</li></ul>';
-	}
-  	return $crumbs;
-}
-
-*/
 
 ?>
